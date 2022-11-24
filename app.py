9347ad04844c1d8a9161784235f5b085d381e846
siddharth-r-tiwari <sid.r.tiwari@gmail.com>
2022-11-23 22:10:34 -0500

    created basic inputs and established routes

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import Flask, flash, redirect, render_template
+
+app = Flask(__name__)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        return redirect("/")
+    else:
+        return render_template("index.html")
